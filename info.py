f79983154826bbd238fa5c3d75e511308c76bb8b
KingOfGhosts <priyanshu020210@gmail.com>
2024-07-21 11:27:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ USERNAME = environ.get('USERNAME', "https://telegram.me/PirateGamers")
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001867240136'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/iPopkarn_Movies')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001850600075').split()]
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Popkarn:Popkarn@cluster0.knhfubs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Popkarn:Popkarn@cluster0.cs0jg1b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 DATABASE_NAME = environ.get('DATABASE_NAME', "Popcorn")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Popcorn')
